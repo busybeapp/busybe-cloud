883c6fffe7c71d9b2b1693cc044f262fa5162dec
Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>
2024-10-01 12:09:16 +0100

    remove redundent comments

--- tests/test_e2e_busybe.py
+++ tests/test_e2e_busybe.py
@@ -36,30 +36,3 @@ def test_all_entries_received(app):
     entry_two = app.create_entry('task 2')
     entries = app.get_entries()
     assert_that(entries, has_items(equal_to(entry_one), equal_to(entry_two)))
-
-
-# def test_create_new_entry_via_slack(app):
-#     title = 'Slack task 1'
-#     response, response_json = app_driver.create_task_via_slack(title)
-#
-#     assert_that(response.status_code, equal_to(200))
-#     assert_that(response_json['text'], equal_to(f"Task created: {title}"))
-#
-#
-# def test_list_tasks_via_slack(app):
-#     app_driver.create_task_via_slack('Slack task 1')
-#     app_driver.create_task_via_slack('Slack task 2')
-#
-#     response, response_json = app_driver.list_tasks_via_slack()
-#     task_list = response_json['text']
-#
-#     assert_that(response.status_code, equal_to(200))
-#     assert_that(task_list, is_('Here are your tasks:\nSlack task 1\nSlack task 2'))
-#
-#
-# def test_invalid_slack_token(app):
-#     title = 'Invalid token task'
-#     response, response_json = app_driver.send_invalid_token("/busybe", title)
-#
-#     assert_that(response.status_code, equal_to(401))
-#     assert_that(response_json['detail'], equal_to("Invalid token"))
