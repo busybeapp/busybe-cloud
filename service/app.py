dd805e95282a37869599324ec6987f3442a4291c
Amazia Gur <149373874+amaziahub@users.noreply.github.com>
2025-01-30 20:47:30 +0000

    fix: Bzb 29 busy be web fail to load entries from server due to https issues (#10)
    * fix: enforce https
    
    * fix: enforce https
    
    ---------
    
    Co-authored-by: Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>

--- service/app.py
+++ service/app.py
@@ -6,6 +6,8 @@ from fastapi.exceptions import RequestValidationError
 from fastapi.responses import JSONResponse
 from starlette import status
 from starlette.middleware.cors import CORSMiddleware
+from starlette.middleware.httpsredirect import HTTPSRedirectMiddleware
+from starlette.responses import RedirectResponse
 
 from service.entries import router as entries_router
 from service.health import router as health_router
@@ -16,6 +18,9 @@ logger = logging.getLogger(__name__)
 
 load_dotenv()
 app = FastAPI()
+
+if os.getenv("ENV") != "testing":
+    app.add_middleware(HTTPSRedirectMiddleware)
 
 app.add_middleware(
     CORSMiddleware,
@@ -37,6 +42,13 @@ async def validation_exception_handler(request: Request, exc: RequestValidationE
 app.include_router(health_router.router, prefix="/health")
 app.include_router(entries_router.router, prefix="/api/entries")
 app.include_router(slack_router.router, prefix="/api/slack/message-shortcut")
+
+
+@app.middleware("http")
+async def enforce_https(request: Request, call_next):
+    if request.headers.get("X-Forwarded-Proto") == "http":
+        return RedirectResponse(request.url.replace(scheme="https"), status_code=301)
+    return await call_next(request)
 
 
 def main():
