8307df801ba9e0d54f42ca84b5a9423a37f5f686
Amazia Gur <149373874+amaziahub@users.noreply.github.com>
2025-01-31 13:45:11 +0000

    refactor: cleanup https tryouts leftovers (#12)
    Co-authored-by: Amazia Gur <amaziagur@Amazias-MacBook-Pro.local>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,4 @@
-import os
-import pytest
 from dotenv import load_dotenv
 
 # Load environment variables from .env file
 load_dotenv()
-
-
-@pytest.fixture(scope="session", autouse=True)
-def set_env_for_testing():
-    os.environ["ENV"] = "testing"
-    load_dotenv(override=True)
-
-    yield
-
-    del os.environ["ENV"]
-    load_dotenv(override=True)
